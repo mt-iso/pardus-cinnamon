3c4b08c0592b734bb2797507fec6ed68bcdbc18f
mobilturka <93243536+mobilturka@users.noreply.github.com>
2023-04-15 11:53:20 +0300

    Update customize-airootfs.sh

--- turka/customize-airootfs.sh
+++ turka/customize-airootfs.sh
@@ -12,14 +12,14 @@ rm -rf /usr/share/backgrounds/gnome
 apt install wget
 
 ### 17g installer
-wget https://github.com/pardus-turka/17g-installer/releases/download/current/17g-installer_1.0_all.deb
+#wget https://github.com/pardus-turka/17g-installer/releases/download/current/17g-installer_1.0_all.deb
 
 ## Mobilturka Wallpapers
 wget https://github.com/mobilturka/mt-wallpapers/releases/download/current/mt-wallpapers_1.0_all.deb
 
 # Extras
-wget https://github.com/mobilturka/debian-firefox-tr/releases/download/latest/firefox-112.0_amd64.deb
-wget https://github.com/troyeguo/koodo-reader/releases/download/v1.5.3/Koodo.Reader-1.5.3-amd64.deb
+#wget https://github.com/mobilturka/debian-firefox-tr/releases/download/latest/firefox-112.0_amd64.deb
+#wget https://github.com/troyeguo/koodo-reader/releases/download/v1.5.3/Koodo.Reader-1.5.3-amd64.deb
 
 
 apt install ./*.deb -yq --allow-downgrades
