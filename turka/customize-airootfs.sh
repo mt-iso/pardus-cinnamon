f9755ffa9f88f7d03171fde88ec9ff70d459f526
mobilturka <93243536+mobilturka@users.noreply.github.com>
2023-04-24 09:34:57 +0300

    Update customize-airootfs.sh

--- turka/customize-airootfs.sh
+++ turka/customize-airootfs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd /tmp
+#cd /tmp
 
 ### Drop no-recommend
 rm /etc/apt/apt.conf.d/01norecommend
@@ -11,6 +11,5 @@ rm -rf /usr/share/backgrounds/gnome
 ### Install packages
 apt install wget -y
 
-wget https://ftp.mozilla.org/pub/firefox/releases/113.0b1/linux-x86_64/en-US/firefox-113.0b1.deb
 
-apt install ./*.deb -yq --allow-downgrades
+#apt install ./*.deb -yq --allow-downgrades
