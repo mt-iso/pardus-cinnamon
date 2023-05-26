755694559733f4e28bd1711e67913f328672a85d
mobilturka <93243536+mobilturka@users.noreply.github.com>
2023-05-26 14:14:31 +0300

    Update customize-airootfs.sh

--- pardus-cinnamon/customize-airootfs.sh
+++ pardus-cinnamon/customize-airootfs.sh
@@ -10,7 +10,7 @@ rm -rf /usr/share/backgrounds/gnome
 
 ### Install packages
 apt install wget -y
-#wget https://github.com/mobilturka/mt-wallpapers/releases/download/current/mt-wallpapers_1.0_all.deb
+wget https://github.com/mobilturka/mt-wallpapers/releases/download/current/mt-wallpapers_1.0_all.deb
 #apt install -t yirmiuc-backports linux-image-amd64 -y
 
 
