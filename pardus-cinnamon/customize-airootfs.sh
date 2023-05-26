8907a0d9e80055892219876991a3636093854728
mobilturka <93243536+mobilturka@users.noreply.github.com>
2023-05-26 14:03:36 +0300

    Add files via upload

---
+++ pardus-cinnamon/customize-airootfs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#cd /tmp
+
+### Drop no-recommend
+rm /etc/apt/apt.conf.d/01norecommend
+
+### Cleaning up excess
+rm -rf /usr/share/backgrounds/gnome
+
+### Install packages
+apt install wget -y
+#wget https://github.com/mobilturka/mt-wallpapers/releases/download/current/mt-wallpapers_1.0_all.deb
+#apt install -t yirmiuc-backports linux-image-amd64 -y
+
+
+#apt install ./*.deb -yq --allow-downgrades
