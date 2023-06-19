ce15179ebbc12b0da3650fd0b589a3a44f59ea5c
mobilturka <93243536+mobilturka@users.noreply.github.com>
2023-06-20 00:51:30 +0300

    Update customize-airootfs.sh

--- pardus-cinnamon/customize-airootfs.sh
+++ pardus-cinnamon/customize-airootfs.sh
@@ -13,7 +13,7 @@ apt install wget -y
 wget http://ftp.debian.org/debian/pool/main/l/linux-signed-amd64/linux-image-6.3.0-1-amd64_6.3.7-1_amd64.deb
 wget https://github.com/mobilturka/mt-wallpapers/releases/download/current/mt-wallpapers_1.0_all.deb
 wget https://github.com/troyeguo/koodo-reader/releases/download/v1.5.5/Koodo.Reader-1.5.5-amd64.deb
-apt install -t yirmibir-backports linux-image-amd64 orchis-gtk-theme papirus-icon-theme -y
+apt install -t yirmibir-backports orchis-gtk-theme papirus-icon-theme -y
 
 
 apt install ./*.deb -yq --allow-downgrades
